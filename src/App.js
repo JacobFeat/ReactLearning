8a5c20914a94b6c68d3a63b9564111a3ed2481f0
Jakub Kita <63514043+JacobFeat@users.noreply.github.com>
2021-11-13 14:50:04 +0100

    add user, delete user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,47 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
+import { Wrapper } from "./components/atoms/Wrapper/Wrapper";
+import { students } from "./data/students";
 
 const initialState = 0;
+const initialForm = {
+  name: "",
+  surname: "",
+  nickname: ""
+};
 
 export default function App() {
   const [count, setCount] = useState(initialState);
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState(initialForm);
+  const [users, setUsers] = useState(students);
+
+  useEffect(() => {
+    document.title = count;
+  });
 
   const handleInput = (e) => {
-    setInputValue(e.target.value);
-    console.log(inputValue);
+    setInputValue({
+      ...inputValue,
+      [e.target.name]: e.target.value
+    });
+    console.log(e.target.name);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    const newUser = {
+      name: `${inputValue.name} ${inputValue.surname} aka ${inputValue.nickname}`
+    };
+    setUsers([newUser, ...users]);
     console.log(inputValue);
+  };
+
+  const deleteUser = (name) => {
+    const filteredUser = users.filter((user) => {
+      return user.name !== name;
+    });
+    setUsers(filteredUser);
+    // console.log(users[0].name);
   };
 
   return (
@@ -25,13 +52,38 @@ export default function App() {
         <button onClick={() => setCount(count - 1)}>-</button>
         <button onClick={() => setCount(count + 1)}>+</button>
       </div>
-      <form onClick={handleSubmit}>
-        <label htmlFor="test">
-          Test:
-          <input id="test" type="text" onChange={handleInput} />
-        </label>
+      <Wrapper as="form" onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          name="name"
+          value={inputValue.name}
+          onChange={handleInput}
+        />
+        <label htmlFor="surname">Surname:</label>
+        <input
+          id="surname"
+          type="text"
+          name="surname"
+          value={inputValue.surname}
+          onChange={handleInput}
+        />
+        <label htmlFor="nickname">Nickname:</label>
+        <input
+          id="nickname"
+          type="text"
+          name="nickname"
+          value={inputValue.nickname}
+          onChange={handleInput}
+        />
         <input type="submit" value="Check" />
-      </form>
+      </Wrapper>
+      <ul>
+        {users.map(({ name }) => (
+          <li onClick={() => deleteUser(name)}>{name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
